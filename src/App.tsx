b988959675bb91fde119b19d1ba98b84ae3efb62
KallNascimento <iksn7@icloud.com>
2022-08-10 19:46:28 -0300

    tailwind config, add components and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import logo from './logo.svg'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 import './App.css'
+import Header from './layout/header'
+import Footer from './layout/footer'
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='flex-1'>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+        </Routes>
+      </Router>
+      <Footer />
+    </section>
+
   )
 }
 
---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard(){
+    return (
+        <>
+        <h1 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Babundo is working now.</h1>
+        </>
+    )
+}
